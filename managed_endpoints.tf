a011006ea15853fb885789cbf8add994b19de171
Dima <velychkodmytro@gmail.com>
2024-04-12 17:53:08 +0300

    fix: changed adls condition

--- managed_endpoints.tf
+++ managed_endpoints.tf
@@ -29,12 +29,12 @@ data "azapi_resource" "this" {
 resource "azapi_update_resource" "adls" {
   for_each = { for i in var.managed_private_endpoint : i.name => i if i.type == "Microsoft.Storage/storageAccounts@2023-01-01" }
 
-  name = data.azapi_resource.this[each.key].output != 0 ? one([
+  name = one([
     for connection in jsondecode(data.azapi_resource.this[each.key].output).properties.privateEndpointConnections
     : connection.name
     if
     endswith(connection.properties.privateLinkServiceConnectionState.description, "${each.key}-${var.env}")
-  ]) : null
+  ])
   type      = "Microsoft.Storage/storageAccounts/privateEndpointConnections@2023-01-01"
   parent_id = each.value.subresource_name == "dfs" ? var.storage_account_id : each.value.target_resource_id
 
@@ -51,6 +51,32 @@ resource "azapi_update_resource" "adls" {
     ignore_changes = all
   }
 }
+
+# resource "azapi_update_resource" "adls" {
+#   for_each = { for i in var.managed_private_endpoint : i.name => i if i.type == "Microsoft.Storage/storageAccounts@2023-01-01" }
+
+#   name = data.azapi_resource.this[each.key].output != 0 ? one([
+#     for connection in jsondecode(data.azapi_resource.this[each.key].output).properties.privateEndpointConnections
+#     : connection.name
+#     if
+#     endswith(connection.properties.privateLinkServiceConnectionState.description, "${each.key}-${var.env}")
+#   ]) : null
+#   type      = "Microsoft.Storage/storageAccounts/privateEndpointConnections@2023-01-01"
+#   parent_id = each.value.subresource_name == "dfs" ? var.storage_account_id : each.value.target_resource_id
+
+#   body = jsonencode({
+#     properties = {
+#       privateLinkServiceConnectionState = {
+#         description = "Approved via Terraform ${azurerm_data_factory_managed_private_endpoint.this["${each.key}-dev"].name}"
+#         status      = "Approved"
+#       }
+#     }
+#   })
+
+#   lifecycle {
+#     ignore_changes = all
+#   }
+# }
 
 # key vault
 resource "azapi_update_resource" "keyvault_approval" {
